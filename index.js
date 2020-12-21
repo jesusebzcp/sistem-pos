eaadb4b480d0b92f987e3faa622ce0dbe5642ac2
jesus <jesusebzcp19@gmail.com>
2020-12-21 17:01:25 -0500

    refactor code order imports

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ connectDb();
 
 app.use(express.json({ extended: true }));
 app.use(cors());
-const port = process.env.port || 4000;
 
 //Create user
 app.use("/api/create-user", require("./src/router/user"));
 app.use("/api/login", require("./src/router/auth"));
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`On server listener port ${port}`);
+app.listen(process.env.PORT || 4000, "0.0.0.0", () => {
+  console.log(`On server listener port ${process.env.PORT}`);
 });
